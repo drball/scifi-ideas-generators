de19db2c17bc257836d99c7651e0eb6d65dd106e
David Ball <david@ongoingworlds.com>
2020-07-17 18:46:35 +0100

    City name generator

---
+++ content/city-name-generator.md
@@ -0,0 +1,12 @@
+---
+title: "City name generator"
+date: 2020-07-17T08:28:49+01:00
+description: "Need a fictional city name? This generator randomly creates unique city names."
+layout: city-name-generator
+---
+
+Need a fictional city name? This generator randomly creates unique city names for you to use in your story, roleplay or game project.
+
+This generator was inspired by place names (towns, villages & cities) from the United Kingdom, America, Australia, Germany, & China. The result is can be quite unnatural, so this can be used as inspiration for alien cities on strange other planets perhaps? 
+
+To generate more city names, press the button.
---
+++ static/js/city-name-generator.js
@@ -0,0 +1,274 @@
+
+
+
+
+function city1(){
+    var options = [
+        "As","Bab","Bac","Back","Bad","Baf","Bag","Bagging","Bai","Bake","Baking","Ban","Bang","Bap","Bar","Barge","Basing",
+        "Beef","Ben","Bic","Bid","Bide","Biden","Big",
+        "Bil","Bin","Bip",
+        "Black",
+        "Black",
+        "Blue",
+        "Boat",
+        "Board",
+        "Bog",
+        "Bol",
+        "Bos",
+        "Bop",
+        "Cal",
+        "Car",
+        "Can",
+        "Carol","Cay",
+        "Cat",
+        "Canal",
+        "Chest","Chic","Chick","Chicken","Chig","Cook","Cor",
+        "Dan","Dang","Dark","Davis","Den","Der","Din","Dog","Don","Dong","Dove","Down","Dram",
+        "Dom","Dres","Druk","Duk","East","Em","Ex",
+        "Far","Fil","Finn","Fitz","Frank","Full","Fuller","Fynn",
+        "Gap",
+        "Gar","George","Gin","Grand","Grave","Gros","Guild",
+        "Gop",
+        "Ham","Han","Hart","Hib","Hic","Hick","High","Hik","Holy","Hor","Horing","Huge","Hugh","Jis","Jiz",
+        "Ken","Kent",
+        "Kit",
+        "Lar","Lard","Large","Leah","Lee","Lie","Lin","Lip",
+        "Lon","Long",
+        "Lot","Lotta","Low","Luck","Lucky","Luke","Lyon","Mam","Man","Mar","Mat","Mauve","Mec","Med","Mem","Men","Mild","Ming","Mom","Mon","Muh","Near","Nord",
+        "North",
+        "North",
+        "Oak","Ox","Pic","Picard","Pip","Rad","Rap","Raptor","Raven","Ray",
+        "Red",
+        "Red",
+        "Rich","Rock","Rod","Rose","Ros","Rot","Sam","San","Sandle","Scot","Short","South","St",
+        "Sud","Summer","Sun",
+        "Sweet",
+        "Swan",
+        "Swamp",
+        "Tal","Ted","Tep","Tib","Tin","Tip","To","Tor","Val","War","We","Wel",
+        "West",
+        "West",
+        "Wet",
+        "Wharf",
+        "White",
+        "White",
+        "Wil",
+        "Winter",
+        "Winter",
+        "Wolf","Won","Wor","Wu","Xin","Yan","Yen","Zin",
+    ];
+
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+function cityMiddle(){
+    var options = [
+        "ab","ag","al","an","ay","er","im","in","ing","og","on","or", "tor","le",
+
+    ];
+
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+function city2() {
+    var options = [
+        "-on-sea",
+        "ack", "ago", "al", "aloo", "ama", "ams", "anne", "ant", "anta", "apolis", "ara", "arva", "arvon", "ary", "bach", "bat",
+        "berg",
+        "berg",
+        "bord", "borne",
+        "bourne",
+        "bourne",
+        "bron",
+        "bron",
+        "burg",
+        "burg",
+        "borg",
+        "burger",
+        "burn",
+        "burton",
+        "burton",
+        "bury",
+        "bury",
+        "by",
+        "sby",
+        "caster", "castle", "chester", "dad", "dale", "dam", "dan", "dem", "den", "dena", "dence", "deno", "dense", "dom", "don",
+        "dorf",
+        "dorn",
+        "down",
+        "dun", "eau", "enna", "enne", "enya",
+        "ertle",
+        "esse",
+        "est",
+        "end",
+        "ends",
+        "fam", "fom", "field", "flow",
+        "ford",
+        "ford",
+        "forn",
+        "fornia",
+        "fort",
+        "fort",
+        "furt",
+        "furt",
+        "gal",
+        "gap",
+        "gana",
+        "gate",
+        "gen",
+        "gin", "gley", "go", "gol",
+        "gon",
+        "ham",
+        "hamm",
+        "han",
+        "hang", "hard",
+        "hole",
+        "hom",
+        "hong",
+        "horn",
+        "houn", "ia", "ian", "in",
+        "ing",
+        "ing",
+        "inne",
+        "ison",
+        "istan",
+        "kon", "kyo", "laix",
+        "lan",
+        "lan",
+        "lane",
+        "lane",
+        "lin",
+        "ling",
+        "land",
+        "land",
+        "lanta", "lay", "lean", "let", "ley", "lina", "ling", "lock", "lok", "lon", "look",
+        "mark",
+        "mary",
+        "men",
+        "mend",
+        "mento",
+        "moor",
+        "moor",
+        "moth",
+        "mouth",
+        "mouth",
+        "mum",
+        "muth",
+        "mund",
+        "nan",
+        "nell",
+        "nes",
+        "nett",
+        "oaks",
+        "oha",
+        "oke",
+        "olina",
+        "oon", "ora", "oria", "otah",
+        "otter",
+        "on",
+        "pen",
+        "polis", "poo", "quay", "rell", "river",
+        "saw",
+        "sburg",
+        "sbury", "sen", "sex", "shut", "sing", "skin", "son", "soon", "steak",
+        "stein",
+        "stein",
+        "stan",
+        "ston",
+        "ston",
+        "stone", "stream", "strem",
+        "tog",
+        "ton",
+        "ton",
+        "tone",
+        "tone",
+        "town",
+        "town",
+        "tucky",
+        "tun",
+        "tun",
+        "turn",
+        "tuna",
+        "tune",
+        "uma", "urt",
+        "vale",
+        "view",
+        "view",
+        "vile",
+        "vile",
+        "ville",
+        "ville",
+        "vis",
+        "walk",
+        "wanna",
+        "ward",
+        "ward",
+        "wark", "wege", "wei", "werk", "werp",
+        "wick",
+        "wick",
+        "wharf",
+        "wood",
+        "wood",
+        "work",
+        "worth",
+        "worth",
+        "worf",
+        "yuan",
+        "zhong",
+
+    ];
+
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+function cityPrefix(){
+    var options = [
+        "East","High","Low","New","North","Old","South","West"
+    ];
+
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+function citySuffix(){
+    var options = [
+        "Bay","Beach","City","Grove","Heights","Hills","Junction","Lake","Point","Springs","Town",
+    ];
+
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+newCityName = function(){
+    var final = "";
+    var typeChance = Math.floor(Math.random() * 100);
+    var prefixChance = Math.floor(Math.random() * 100);
+
+    if(typeChance < 15){
+        final =  planetName();
+    } else if (typeChance >= 15 && typeChance < 45) {
+        final = city1() + cityMiddle() + city2();
+    } else {
+        final = city1() + city2();
+    }
+
+    if(prefixChance < 20){
+        final =  cityPrefix() + " " + final;
+    } else if (typeChance >= 20 && typeChance < 40) {
+        final =  final + " " + citySuffix();
+    }
+
+    return final;
+}
+
+generateNewCityNames = function(){
+    $(".city-name").each(function(){
+
+        $(this).html("");
+
+        $(this).prepend(newCityName());
+
+    });
+}
+
+$(document).ready(function() {
+    generateNewCityNames();
+});
